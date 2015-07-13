d94a9233954d3fd68e28f7b5fe2a23fef2bbbcd7
Jonathan A. Gross <jarthurgross@gmail.com>
2015-07-13 11:14:25 -0600

    Add mock numpy module so RTD will build documentation

--- doc/conf.py
+++ doc/conf.py
@@ -16,6 +16,18 @@
 import sys
 import os
 
+# Need some mock modules since readthedocs doesn't have python-dev installed, so
+# it can't install numpy
+from mock import Mock as MagicMock
+
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return Mock()
+
+MOCK_MODULES = ['numpy']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
